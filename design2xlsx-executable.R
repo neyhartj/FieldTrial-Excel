112d2039764a7be1f385c9b2bd75b5ccccf07e27
neyhartj <neyha001@umn.edu>
2016-01-23 14:23:06 -0600

    Removed unnecessary files

--- design2xlsx-executable.R
+++
@@ -1,9 +0,0 @@
-##Executing design2xlsx
-
-setwd("C:/Users/Jeff/Documents/University of Minnesota/Barley Lab/Projects/Repositories/plotdesign-excel")
-
-source("design2xlsx-FUNCTION.R")
-
-load("plot_design_excel_data.RData")
-
-design2xlsx(x = Designs_List[[1]], NumLocs = F, FieldDim = F, NumDupl = F)
